38d5d44b66202967215cb9a3f6ee84bc88c54de9
Roopam Jain <roopam.ams@gmail.com>
2021-05-25 05:59:26 +0530

    initial commit

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+console.log("Client side javascript file is loaded")
+
+const weatherForm = document.querySelector("form")
+const search = document.querySelector("input")
+const messageOne = document.getElementById("message-1")
+const messageTwo = document.getElementById("message-2")
+
+weatherForm.addEventListener("submit", (e) => {
+  e.preventDefault()
+
+  const location = search.value
+
+  fetch("http://localhost:3000/weather?address=" + location).then(
+    (response) => {
+      if (!location) {
+        return console.log("You must provide a location!")
+      }
+
+      messageOne.textContent = "Loading..."
+      messageTwo.textContent = ""
+
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error
+        } else {
+          messageOne.textContent = data.location
+          messageTwo.textContent = data.forecast
+        }
+      })
+    }
+  )
+})
